de67bed27854d791113c6f22340db9fd494c124a
IsmailKemmoune <i.kemmoune@gmail.com>
2023-05-04 21:03:22 +0100

    Feat (Game memory logic): created the time generator function and format it

--- index.js
+++ index.js
@@ -28,3 +28,25 @@ const items = [
   { name: "the-philosopher-s-stone", image: ".images/the-philosopher-s-stone" },
   { name: "the-prisoner-of-azkaban", image: ".images/the-prisoner-of-azkaban" },
 ];
+
+//Initial Time
+let seconds = 0,
+  minutes = 0;
+//Initial moves and win count
+let movesCount = 0,
+  winCount = 0;
+
+const timeGenerator = () => {
+  seconds += 1;
+  //minutes logic
+  if (seconds >= 60) {
+    minutes += 1;
+    seconds = 0;
+  }
+  //format time before displaying
+  let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+  let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+  timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+};
+
+timeGenerator();
